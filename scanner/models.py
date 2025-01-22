0a234424fae0043f521ab48ba4ef2aa6677139b7
Utkarsh <utkarsh.cpp@gmail.com>
2025-01-22 13:50:14 +0530

    Added values to scanner_valuemapping

--- scanner/models.py
+++ scanner/models.py
@@ -30,7 +30,7 @@ class AzureDeployment(models.Model):
     
 class ValueMapping(models.Model):
     attack_name = models.CharField(max_length=255)  # Stores the name of the attack
-    probes_name = models.CharField(max_length=255)  # Stores the name of the probe
+    probes_name = models.CharField(max_length=500)  # Stores the name of the probe
 
     def __str__(self):
         return f"{self.attack_name} -> {self.probes_name}"
