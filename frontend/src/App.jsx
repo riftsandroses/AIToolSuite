432bf80f1819b1677e44828abd4d6876959b9a68
Saptarshee Mitra <saptarshee08@gmail.com>
2025-01-28 19:09:24 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+// import './App.css';
+import TextField from '@mui/material/TextField';
+
+function App() {
+  return (
+    <div className="bg-red-50 h-[10rem] w-[10rem]">
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </div>
+  );
+}
+
+export default App;
